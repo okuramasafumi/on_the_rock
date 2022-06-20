075612f311383c448459268c19cdad9e020a489e
OKURA Masafumi <masafumi.o1988@gmail.com>
2022-06-20 23:08:05 +0900

    Initial commit

---
+++ lib/on_the_rock.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "on_the_rock/version"
+
+module OnTheRock
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/on_the_rock/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module OnTheRock
+  VERSION = "0.1.0"
+end
